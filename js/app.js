7506e4f6f918535c84b988b89d00d7401a53b0a5
fahadMridha <sohelcse16@gmail.com>
2022-09-02 22:50:25 +0600

    categories added by daynamacially

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('hello from js')
+const loadAllCetagorys = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url)
+    const data = await res.json();
+    seatAllCetagorys(data.data.news_category)
+}
+const seatAllCetagorys = (categories) => {
+    const setCategories = document.getElementById('all-category');
+    for (const categorie of categories) {
+        // console.log(categorie.category_name)
+        const li = document.createElement("li");
+        // li.classList.add('text-decoration-none')
+        li.innerHTML = `<a >${categorie.category_name}</a>`;
+        setCategories.appendChild(li);
+    }
+}
+loadAllCetagorys()
